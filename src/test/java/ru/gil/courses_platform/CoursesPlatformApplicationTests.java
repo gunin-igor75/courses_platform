e36e4f94aeb7a6618d9ae2a2cea7bfac14af1f9f
Igor Gunin <gunin-igor75@yandex.ru>
2023-06-08 08:35:18 +0600

    first commit

---
+++ src/test/java/ru/gil/courses_platform/CoursesPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gil.courses_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
